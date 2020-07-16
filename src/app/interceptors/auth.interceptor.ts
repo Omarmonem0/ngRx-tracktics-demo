91985db8fe2d62b18492282c7dc28d6c89890377
Omaromaromar@1996 <omarahmedmonem@gmail.com>
2020-07-16 08:33:12 +0200

    remove token

--- src/app/interceptors/auth.interceptor.ts
+++ src/app/interceptors/auth.interceptor.ts
@@ -15,7 +15,7 @@ export class AuthHandler implements HttpInterceptor {
     }
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         // tslint:disable-next-line: max-line-length
-        const token = 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOi8vdHJhY2t0aWNzLnpvbmUiLCJqdGkiOiJBZ0xmeW85TyIsImlhdCI6MTU5NDgzNzc1NiwiZXhwIjoxNTk0ODQxMzU2LCJ1c2VySWQiOiJzdDdZYyIsInJvbGVJZCI6MjAwLCJpc1JlZnJlc2hUb2tlbiI6ZmFsc2V9.NaoyZ3bMJpin8xb2M9F1unekUv3NrEJESeMB0DaJSdM';
+        const token = 'Bearer ';
         const sessionReq = request.clone({
             headers: request.headers.set('AUTHORIZATION' , token)
           });
